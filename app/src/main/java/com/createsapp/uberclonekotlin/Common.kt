4559b63c9e1c4658eed5c20feb23b9c0daafbde5
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-07-13 19:50:29 +0530

    Driver Presentence Driver

--- app/src/main/java/com/createsapp/uberclonekotlin/Common.kt
+++ app/src/main/java/com/createsapp/uberclonekotlin/Common.kt
@@ -3,6 +3,7 @@ package com.createsapp.uberclonekotlin
 import com.createsapp.uberclonekotlin.model.DriverInfoModel
 
 object Common {
+    val DRIVER_LOCATION_REFERENCE: String = "DriversLocation"
     var currentUser: DriverInfoModel? = null
     val DRIVER_INFO_REFERENCE: String = "DriverInfo"
 }
--- app/src/main/java/com/createsapp/uberclonekotlin/ui/home/HomeFragment.kt
+++ app/src/main/java/com/createsapp/uberclonekotlin/ui/home/HomeFragment.kt
@@ -12,7 +12,10 @@ import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.createsapp.uberclonekotlin.Common
 import com.createsapp.uberclonekotlin.R
+import com.firebase.geofire.GeoFire
+import com.firebase.geofire.GeoLocation
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -20,6 +23,9 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
@@ -39,9 +45,38 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
     private lateinit var locationCallback: LocationCallback
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
+    //Online system
+    private lateinit var onlineRef: DatabaseReference
+    private lateinit var currentUserRef: DatabaseReference
+    private lateinit var driverLocationRef: DatabaseReference
+    private lateinit var geoFire: GeoFire
+
+    private val onlineValueEventListener = object : ValueEventListener {
+        override fun onCancelled(error: DatabaseError) {
+            Snackbar.make(mapFragment.requireView(), error.message, Snackbar.LENGTH_LONG).show()
+        }
+
+        override fun onDataChange(snapshot: DataSnapshot) {
+            if (snapshot.exists())
+                currentUserRef.onDisconnect().removeValue()
+        }
+
+    }
+
     override fun onDestroy() {
         fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        geoFire.removeLocation(FirebaseAuth.getInstance().currentUser!!.uid)
+        onlineRef.removeEventListener(onlineValueEventListener)
         super.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        registerOnlineSystem()
+    }
+
+    private fun registerOnlineSystem() {
+        onlineRef.addValueEventListener(onlineValueEventListener)
     }
 
     override fun onCreateView(
@@ -63,6 +98,19 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
     }
 
     private fun init() {
+
+        onlineRef = FirebaseDatabase.getInstance().reference.child(".info/connected")
+        driverLocationRef =
+            FirebaseDatabase.getInstance().getReference(Common.DRIVER_LOCATION_REFERENCE)
+        currentUserRef =
+            FirebaseDatabase.getInstance().getReference(Common.DRIVER_LOCATION_REFERENCE).child(
+                FirebaseAuth.getInstance().currentUser!!.uid
+            )
+
+        geoFire = GeoFire(driverLocationRef)
+
+        registerOnlineSystem()
+
         locationRequest = LocationRequest()
         locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         locationRequest.fastestInterval = 3000
@@ -78,9 +126,32 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
                     locationResult.lastLocation.longitude
                 )
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPos, 18f))
+
+                //Update Location
+                geoFire.setLocation(
+                    FirebaseAuth.getInstance().currentUser!!.uid,
+                    GeoLocation(
+                        locationResult.lastLocation.latitude,
+                        locationResult.lastLocation.longitude
+                    )
+                ) { key: String?, error: DatabaseError? ->
+                    if (error != null)
+                        Snackbar.make(
+                            mapFragment.requireView(),
+                            error.message,
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    else
+                        Snackbar.make(
+                            mapFragment.requireView(),
+                            "You're online!",
+                            Snackbar.LENGTH_SHORT
+                        ).show()
+                }
             }
         }
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context!!)
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
         fusedLocationProviderClient.requestLocationUpdates(
             locationRequest,
             locationCallback,
