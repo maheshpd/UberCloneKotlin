1cba90129842df69a6e154455cdd6b87035169f5
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-07-14 19:01:14 +0530

    Driver Information Panel

--- app/src/main/java/com/createsapp/uberclonekotlin/Common.kt
+++ app/src/main/java/com/createsapp/uberclonekotlin/Common.kt
@@ -3,6 +3,14 @@ package com.createsapp.uberclonekotlin
 import com.createsapp.uberclonekotlin.model.DriverInfoModel
 
 object Common {
+    fun buildWelcomeMessage(): String {
+        return StringBuilder("Welcome, ")
+            .append(currentUser!!.firstname)
+            .append(" ")
+            .append(currentUser!!.lastName)
+            .toString()
+    }
+
     val DRIVER_LOCATION_REFERENCE: String = "DriversLocation"
     var currentUser: DriverInfoModel? = null
     val DRIVER_INFO_REFERENCE: String = "DriverInfo"
